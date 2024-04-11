146e3801b4b1cff55ecea1c76880319cd9ed4426
Carlos Santana <csantana23@gmail.com>
2024-04-11 09:31:49 -0400

    replaced domain
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- hack/run-devopsday-local.sh
+++ hack/run-devopsday-local.sh
@@ -4,5 +4,5 @@ export CNOE_HOST=devopsday.demo.cloud-native-start.com
 #./replace.sh cnoe.localtest.me 8443
 set -x
 
-idpbuilder create --use-path-routing --package-dir ${REPO_ROOT}/examples/ref-implementation-devopsday
+idpbuilder create --use-path-routing --package-dir ${REPO_ROOT}/examples/ref-implementation-devopsday-local
 
